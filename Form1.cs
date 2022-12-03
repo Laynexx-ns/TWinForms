6606e00537ebb8b1f0513ade8818d1fa836d630d
Laynexx-ns <argo150808@gmail.com>
2022-12-03 11:35:31 +0300

    Added visuality functions

--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,10 @@ namespace TWinForms
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("HELLO");
+        }
     }
 }
